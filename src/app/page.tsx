5c44f1d12a2805dbaaa2078a0be66578c4883839
Siddharth27Roy <siddharth27.roy27@gmail.com>
2024-03-03 13:05:44 +0530

    added card and sticky scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-800 antialiased bg-grid-white/[0.02]">
       <HeroSection/>
+      <FeaturedCourses/>
+      <WhyChooseUs/>
     </main>
   );
 }
